0c7501a83a7c863159bec9890adb47c981d573fe
Filiberka <Filiberka@DESKTOP-E7MVDB9>
2020-11-19 14:52:12 +0100

    ...working fix

--- LouisPhilippe_Loeckx_SwapArray/Program.cs
+++ LouisPhilippe_Loeckx_SwapArray/Program.cs
@@ -1,51 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LouisPhilippe_Loeckx_SwapArray
 {
     class Program
     {
 
-
-        class ArrayValues
-        {
-            public int[] FirstArray;
-            public int[] SecondArray;
-
-            public void Show(int[] FirstArray, int[] SecondArray)
-            {
-                foreach (var ArrayOneNumber in FirstArray)
-                {
-                    Console.Write(ArrayOneNumber + " ");
-                }
-                foreach (var item in SecondArray)
-                {
-                    Console.Write(item + " ");
-                }
-            }
-
-            public void Switch()
-            {
-                this.FirstArray = SecondArray;
-                this.SecondArray = FirstArray;
-            }
-        }
-
         static void Main(string[] args)
-        {
-            ArrayValues Arrays = new ArrayValues();
+        {            
             int[] firstArray = { 1,2,3,4,5,6 };
             int[] secondArray = { 6,5,4,3,2,1 };
-            Arrays.FirstArray = firstArray;
-            Arrays.SecondArray = secondArray;
-            Arrays.Show(Arrays.FirstArray, Arrays.SecondArray);
-            Arrays.Switch();
-            Console.WriteLine();
-            Arrays.Show(Arrays.FirstArray, Arrays.SecondArray);
 
+            foreach (var ArrayOneNumber in firstArray)
+            {
+                Console.Write(ArrayOneNumber + " ");
+            }
+            foreach (var item in secondArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                firstArray[i] ^= secondArray[i];
+                secondArray[i] ^= firstArray[i];
+                firstArray[i] ^= secondArray[i];
+
+            }
+
+            Console.WriteLine();
+
+            foreach (var ArrayOneNumber in firstArray)
+            {
+                Console.Write(ArrayOneNumber + " ");
+            }
+            foreach (var item in secondArray)
+            {
+                Console.Write(item + " ");
+            }
 
         }
 
